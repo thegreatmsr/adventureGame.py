4822be61c18f6a16e298d0120228a163ebf70e85
Manav S. Rajput <theexplainer007@gmail.com>
2025-04-25 23:31:22 +0530

    Add files via upload

---
+++ adventureGame.py
@@ -0,0 +1,30 @@
+name = input("Type your name:")
+print("Welcome", name, "To this adventure!")
+
+answer=input("You're on a dirty road, it has come to an end and you can go left or right:" )
+
+
+
+if answer == "left":
+    answer= input("You come to a river, you can walk around it or swim across it, Type Swim to swim or Walk to walk" )
+    if answer == "Swim":
+        print()
+    
+    elif answer == "Walk":
+        print()
+        
+    else:
+          print("Not a vaalid option, Try Better Next Time")
+
+        
+    
+    
+    
+elif answer =="right":
+    print()
+
+else:
+    print("Not a vaalid option, Try Better Next Time")
+
+
+
